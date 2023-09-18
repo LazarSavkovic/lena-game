4d19c0051915cef1429d416ddf94862ca0a06143
Lazar Savkovic <savkoviclazar1@gmail.com>
2023-09-18 19:54:46 +0200

    scroll camera, tiles and game over

--- app.js
+++ app.js
@@ -1,3 +1,52 @@
+const tiles = [[-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+[-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 15, -1, -1, -1, 15, -1, -1, -1, 12, 13, 13, 13, 13, 13, 13, 14, -1, -1, -1, -1, 15, -1, -1, -1, -1, 15, -1, -1, -1, -1, 15, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 15, -1, -1, -1, -1, 15, -1, -1, -1, -1, 15, -1, -1, -1, -1, 12, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 14, -1, -1, -1, -1, 3, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+[-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 15, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 15, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 7, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+[-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 15, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 15, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 7, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+[-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 7, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+[-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 7, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+[-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0, 1, 1, 2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 11, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 15, -1, -1, -1, 15, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+[-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0, 1, 1, 2, -1, -1, 4, 5, 5, 6, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 3, -1, -1, -1, 3, -1, -1, -1, -1, 3, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1],
+[-1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 4, 5, 5, 6, -1, -1, 4, 5, 5, 6, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 12, 13, 13, 13, 14, -1, -1, -1, -1, 12, 13, 13, 13, 14, -1, -1, -1, -1, 0, 2, -1, -1, -1, -1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 2, -1, -1, -1, -1, 7, -1, -1, -1, 7, -1, -1, -1, -1, 7, -1, -1, -1, -1, 0, 1, 1, 2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 0, 1, 1, 1, 1, 1, 1, 1],
+[-1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, -1, -1, 4, 5, 5, 6, -1, -1, 4, 5, 5, 6, -1, -1, -1, -1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 4, 6, -1, -1, -1, -1, 4, 5, 5, 5, 5, 5, 5, 5, 5, 6, -1, -1, -1, -1, 7, -1, -1, -1, 7, -1, -1, -1, -1, 7, -1, -1, -1, -1, 4, 5, 5, 6, -1, -1, -1, -1, 0, 1, 1, 2, -1, -1, -1, -1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 2, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 4, 5, 5, 5, 5, 5, 5, 5],
+[-1, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, -1, -1, 4, 5, 5, 6, -1, -1, 4, 5, 5, 6, -1, -1, -1, -1, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 4, 6, -1, -1, -1, -1, 4, 5, 5, 5, 5, 5, 5, 5, 5, 6, -1, -1, -1, -1, 7, -1, -1, -1, 7, -1, -1, -1, -1, 7, -1, -1, -1, -1, 4, 5, 5, 6, -1, -1, -1, -1, 4, 5, 5, 6, -1, -1, -1, -1, 4, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 5, 6, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 4, 5, 5, 5, 5, 5, 5, 5]]
+
+
+
+
+function createTiles() {
+
+    const otherPlatforms = [];
+    for (let row = 0; row < tiles.length; row++) {
+        for (let cell = 0; cell < tiles[row].length; cell++) {
+
+            if (tiles[row][cell] !== -1) {
+                if (((tiles[row][cell] - 1) && ((tiles[row][cell] - 1) === -1)) || (tiles[row][cell] - 1)) {
+
+                    otherPlatforms.push({
+                        x: cell,
+                        y: row,
+                        width: 1,
+                    })
+                } else {
+
+                    otherPlatforms[otherPlatforms.length - 1].width += 1;
+                }
+
+            }
+
+        }
+    }
+    for (let platform of otherPlatforms) {
+        platform.x = platform.x * 64;
+        platform.y = platform.y * 64;
+        platform.width = platform.width * 64;
+        platform.height = 64;
+    }
+
+    return otherPlatforms;
+}
+const otherPlatforms = createTiles()
+
 const playerSpritesheet = new Image();
 playerSpritesheet.src = './graphics/spritesheet.png'
 const playerSpritesheetFlipped = new Image();
@@ -31,34 +80,21 @@ const c = canvas.getContext('2d')
 const JUMP_SPEED = 22;
 const MOVE_SPEED = 5;
 const GRAVITY = 1;
+let scroll = 0;
+const playerStartingX = 100;
 
 
 const animationFrameLimit = 1;
 let animationFrameCurrent = 0;
 
 const player = {
-    x: 100,
-    y: 400,
+    x: playerStartingX,
+    y: 300,
     height: 128,
     width: 128,
     vy: 0,
     isJumping: false,
     lookingRight: true
-}
-
-
-
-const ground = {
-    x: 50,
-    y: 600,
-    height: 256,
-    width: 1500
-}
-const platform = {
-    x: 450,
-    y: 370,
-    height: 64,
-    width: 1000
 }
 
 
@@ -84,23 +120,20 @@ function drawBackground() {
 
     c.fillStyle = '#67bae0';
     c.fillRect(0, 0, window.innerWidth, window.innerHeight)
+    c.fillStyle = ' #064273';
+    c.fillRect(0, window.innerHeight * 2/3 , window.innerWidth, window.innerHeight / 3)
 
 
 }
 
-function drawGround() {
-
-    c.fillStyle = 'green';
-    c.fillRect(ground.x, ground.y, ground.width, ground.height)
-
+function drawTiles() {
+    for (let tile of otherPlatforms) {
+        c.fillStyle = 'green';
+        c.fillRect(tile.x - scroll, tile.y, tile.width, 64)
+    }
 }
-function drawPlatform() {
-
-    c.fillStyle = 'green';
-    c.fillRect(platform.x, platform.y, platform.width, platform.height)
 
 
-}
 function drawClouds() {
 
     cloudDrift += 0.3;
@@ -122,7 +155,7 @@ function drawPlayer() {
             0,
             spriteWidth,
             spriteHeight,
-            player.x,
+            player.x - scroll,
             player.y,
             player.width,
             player.height
@@ -135,7 +168,7 @@ function drawPlayer() {
             0,
             spriteWidth,
             spriteHeight,
-            player.x,
+            player.x - scroll,
             player.y,
             player.width,
             player.height
@@ -182,11 +215,16 @@ window.addEventListener('keyup', (e) => {
 function continuousMovement() {
     if (keyState['ArrowRight']) {
         player.x += MOVE_SPEED
+        detectHorizontalCollision()
         animate()
     }
     if (keyState['ArrowLeft']) {
         player.x -= MOVE_SPEED
+        detectHorizontalCollision()
         animate()
+    }
+    if (!keyState['ArrowRight'] && !keyState['ArrowLeft']) {
+        currentFrame = 0;
     }
     requestAnimationFrame(continuousMovement)
 }
@@ -208,6 +246,14 @@ function animate() {
 
 function gameLoop() {
 
+    scroll = player.x - playerStartingX * 3;
+
+    if (checkYDeath()) {
+        console.log('game over')
+        showGameOver();
+        return;
+    };
+
     c.clearRect(0, 0, window.innerWidth, window.innerHeight)
 
 
@@ -215,28 +261,68 @@ function gameLoop() {
     player.vy += GRAVITY;
 
 
-    detectCollision()
+    detectVerticalCollision()
 
 
 
 
     drawBackground()
     drawClouds()
-    drawGround()
     drawPlayer()
-    drawPlatform()
+    drawTiles()
 
 
     requestAnimationFrame(gameLoop)
 }
 
+function checkYDeath() {
+    if (player.y > 3000) {
+        return true
+    }
+}
+
+function showGameOver() {
+    const gameOverDiv = document.createElement('div');
+    gameOverDiv.className = 'gameOverDiv'
+    gameOverDiv.innerHTML = `<h2 class='gameOverHeading'>GAME OVER</h2><button id='restartBtn'>restart</button>`
+    const restartBtn = gameOverDiv.querySelector('#restartBtn')
+    restartBtn.onclick = () => {
+        location.reload()
+    }
 
 
+    document.body.append(gameOverDiv)
+}
 
-function detectCollision() {
+
+function detectHorizontalCollision() {
 
 
-    for (let rect of [ground, platform]) {
+    for (let rect of otherPlatforms) {
+
+        if (
+            player.x + 30 < rect.x + rect.width &&
+            player.x + player.width - 30 > rect.x &&
+            player.y + player.height > rect.y &&
+            player.y + 35 < rect.y + rect.height
+        ) {
+
+            // check for left collision
+            if (player.lookingRight) {
+                console.log(' right side')
+                player.x = rect.x - (player.width - 30);
+
+            } else {
+                console.log(' left side')
+                player.x = rect.x + rect.width - 30;
+            }
+        }
+    }
+}
+function detectVerticalCollision() {
+
+
+    for (let rect of otherPlatforms) {
 
         if (
             player.x + 30 < rect.x + rect.width &&
@@ -256,23 +342,6 @@ function detectCollision() {
 
             }
 
-            // Check for side collisions
-            if (
-                player.y + 30 < rect.y + rect.height &&
-                player.y + player.height - 30 > rect.y &&
-                player.x + player.width > rect.x &&
-                player.x < rect.x + rect.width
-            ) {
-                console.log('side')
-                if (player.x < rect.x + rect.width / 2) {
-                    // Player is colliding from the left side of the rectangle
-                    player.x = rect.x - player.width;
-                } else {
-                    // Player is colliding from the right side of the rectangle
-                    player.x = rect.x + rect.width;
-                }
-            }
-
         }
     }
 }
@@ -280,6 +349,3 @@ function detectCollision() {
 
 
 gameLoop()
-
-
-console.log(canvas)
